0c0a170dc7fa2d0cc9e59236295267901f9639a9
Marion FDE <marion.favre-d-echallens@student.ecp.fr>
2018-11-10 17:18:32 +0100

    Essais de lecture

--- challenge.R
+++ challenge.R
@@ -8,4 +8,18 @@
 # setwd("~/GitHub/Challenge_MLC")
 # render("Challenge.Rmd")
 
+library(h5)
 
+data_folder = "C:/Users/Admin/Documents/Centrale Paris/3A/OMA/Machine Learning/Challenge/Data/"
+ytrain = read.csv(paste0(data_folder,"train_y.csv"))
+xtrain = h5file(paste0(data_folder,"train.h5/train.h5"))
+list.datasets(xtrain)
+list.groups(xtrain)
+list.attributes(xtrain)
+
+xeeg1 =  xtrain["/eeg1"]
+xeeg1[]
+
+list.datasets(xtrain)
+
+h5close(xtrain)
