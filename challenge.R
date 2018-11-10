0ce64a8671687717ac658795a2d54fb7ddea4ccc
Marion FDE <marion.favre-d-echallens@student.ecp.fr>
2018-11-10 13:52:55 +0100

    Intro

--- challenge.R
+++ challenge.R
@@ -1,7 +1,11 @@
-library(rmarkdown)
-render("test_rmarkdown.Rmd")
-
 # installer tinytex pour generation de pdf
 # install.packages('tinytex')
 # tinytex::install_tinytex()
 # tinytex:::is_tinytex()
+
+# Generer le pdf
+# library(rmarkdown)
+# setwd("~/GitHub/Challenge_MLC")
+# render("Challenge.Rmd")
+
+
