21597b55bebd000a761936acf09b421caf1a9920
Marion FDE <marion.favre-d-echallens@student.ecp.fr>
2018-11-20 15:00:20 +0100

    RF

--- ent_wavelet.R
+++ ent_wavelet.R
@@ -1,6 +1,7 @@
 library(h5,warn.conflicts = FALSE)
 library(wmtsa)
 library(seewave)
+library(randomForest)
 
 data_folder = "C:/Users/Admin/Documents/Centrale Paris/3A/OMA/Machine Learning/Challenge/Data/"
 ytrain = read.csv(paste0(data_folder,"train_y.csv"))
@@ -9,10 +10,10 @@ xtrain = h5file(name = paste0(data_folder,"train.h5/train.h5"))
 xtest = h5file(name = paste0(data_folder,"test.h5/test.h5"))
 
 eeg1 = as.data.frame(readDataSet(xtrain[list.datasets(xtrain, recursive = TRUE)[4]]))
-x = eeg1[4,]
+x = eeg1[38,]
 plot(as.numeric(x),type="l",ylab="Amplitude en uV")
 
-#decomposition continue en vaguelettes
+#decomposition continue en vaguelettes: filtrer les données
 d = wavCWT(as.numeric(x))
 d_m = as.data.frame(as.matrix(d))
 plot(d)
@@ -33,41 +34,77 @@ entropie1 = function(x){
   return (-ent)  
 }
 
+nonvide = function(x){
+  if (length(x) == 0)
+    return(0)
+  else
+    return(x)
+}
+
 wavelet = function(x)
 {
   pic = as.data.frame(fpeaks(seewave::spec(as.numeric(x),f = 50,plot = FALSE),plot = FALSE))
   pic$freq = pic$freq*1000
   #alpha waves
   f_alpha =  subset(pic,pic$freq >= 8 & pic$freq <= 13)
-  alpha = entropie1(f_alpha[,1])
+  alpha = nonvide(entropie1(f_alpha[,1]))
   
   #theta waves
   f_theta =  subset(pic,pic$freq >= 4 & pic$freq <= 8)
-  theta = entropie1(f_theta[,1])
+  theta = nonvide(entropie1(f_theta[,1]))
   
   #delta waves
   f_delta =  subset(pic,pic$freq >= 0.5 & pic$freq <= 4)
-  delta = entropie1(f_delta[,1])
+  delta = nonvide(entropie1(f_delta[,1]))
   
   #beta waves
   f_beta =  subset(pic,pic$freq >= 13 & pic$freq <= 30)
-  beta = entropie1(f_beta[,1])
+  beta = nonvide(entropie1(f_beta[,1]))
   
   #K-complex
   f_K =  subset(pic,pic$freq >= 0.5 & pic$freq <= 1.5)
-  K = entropie1(f_K[,1])
+  K = nonvide(entropie1(f_K[,1]))
   
   #spindles
   f_sp =  subset(pic,pic$freq >= 12 & pic$freq <= 14)
-  sp = entropie1(f_sp[,1])
+  sp = nonvide(entropie1(f_sp[,1]))
   
   return(c(alpha,beta,delta,theta,K,sp))
 }
 
 wavelet(x)
 s = apply(eeg1,1,wavelet)
-sd = as.data.frame(t(s))
-colnames(sd) = c("alpha1_ent1","beta1_ent1","delta1_ent1","theta1_ent1","K1_ent1","sp1_ent1")
-sd = cbind(ytrain,sd)
+df = 
+colnames(df)[3:ncol(df)] = c("alpha1_ent1","beta1_ent1","delta1_ent1","theta1_ent1","K1_ent1","sp1_ent1")
+           
 write.csv(sd,file = paste0(data_folder,"wavelets_eeg1.csv"),row.names = FALSE)
+
+#####RandomForest
+
+erreur_mat = function(ytrue,yhat){
+  #matrice de confusion
+  M = table(y =ytrue, yhat)
+  print(M)
+  
+  #soit entre terme de taux d'erreur :
+  return(1-sum(diag(M))/sum(M))
+}
+
+df$sleep_stage = as.factor(df$sleep_stage)
+df_train = df[1:25526,] #2/3
+df_test = df[25526:nrow(df),]
+f_RandomForest = randomForest(sleep_stage~.,data=df_train[,2:ncol(df)])
+print(f_RandomForest)
+
+#Variables d'importance 
+imp = as.data.frame(f_RandomForest$importance[order(f_RandomForest$importance[, 1], 
+                                                    decreasing = TRUE), ])
+
+f2_RandomForest = randomForest(sleep_stage~.,data=df_train[,c("sleep_stage",rownames(imp)[1:10])])
+print(f2_RandomForest)
+yhat = as.data.frame(predict(f2_RandomForest,df_test[,3:ncol(df_test)]))
+erreur_mat(df_test[,2],yhat[,1])
+
+
+
 
