1273f8bf0776321d0b8fa78cb3d3e14c43e68eca
Marion FDE <marion.favre-d-echallens@student.ecp.fr>
2018-11-25 11:12:13 +0100

    improvements

--- source.R
+++ source.R
@@ -27,13 +27,34 @@ f_RandomForest = randomForest(sleep_stage~.,data=df[,2:ncol(df)])
 print(f_RandomForest)
 
 #Variables d'importance 
-imp = as.data.frame(f_RandomForest$importance[order(f_RandomForest$importance[, 1], 
+imp = as.data.frame(f_RandomForest3$importance[order(f_RandomForest3$importance[, 1], 
                                                     decreasing = TRUE), ])
 
 
+#better model ?
+f_RandomForest2 = randomForest(sleep_stage~.,
+                               data=df[,c("sleep_stage",rownames(imp)[1:35])],ntree=800)
+print(f_RandomForest2)
+
+basic = read.csv(paste0(data_folder,"basic_feat.csv"))[,c(3,4,5,6,7,8,23,24)]
+df2 = cbind(df[,c("sleep_stage",rownames(imp)[1:35])],basic)
+f_RandomForest3 = randomForest(sleep_stage~.,
+                               data=df2,ntree=1000)
+print(f_RandomForest3) ### nul
 #prediction
 dft = rassembler_feat(train = FALSE)
 ytest = as.data.frame(predict(f_RandomForest2,dft[,rownames(imp)[1:35]]))
 ytest = cbind(yrandom[,1],ytest)
 colnames(ytest) =  c("id","sleep_stage")
 write.csv(ytest,file = paste0(data_folder,"ytest_w_coeff.csv"),row.names = FALSE)
+
+
+
+#### améliorations possibles:
+# - filtrer les signaux avant de calculer les features
+# - decomposer en moins de vaguelettes
+# - tester svm et adaboost
+# - calculer d'autres features' entropie de Renyi
+# - caracteriser le stade 1 qui est oour l'instant ' inclassable
+# - frequences ?
+
