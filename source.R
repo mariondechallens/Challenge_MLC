351a29b331ed048fd271b40309bddcdbbe3f8358
Marion FDE <marion.favre-d-echallens@student.ecp.fr>
2018-12-09 17:18:51 +0100

    Rmd

--- source.R
+++ source.R
@@ -37,7 +37,7 @@ calcul_feat_wavelets_bis(xtest,train = FALSE)
 dfw = rassembler_feat()  ##ent Ren et sd sur vaguelettes
 #dfw = rassembler_feat_wave3()  ##ent Renyi et mean abs sur vaguelettes apres filtrage 0 30
 df_abs = read.csv(paste0(data_folder,"basic_abs.csv"))
-# dff = rassembler_feat_prop()
+dfp = rassembler_feat_prop()
 dfa = rassembler_feat_alpha()
 # sd_acc = read.csv(paste0(data_folder,"basic_feat.csv")) [,c(1,2,4,6,8,24)]
 # 
@@ -48,7 +48,7 @@ df[is.na(df)] = 0 #setting NA values to zero
 df = merge(df,dfw, by =c("id","sleep_stage"),all.x = TRUE, all.y = TRUE)
 df = merge(df,df_abs, by =c("id","sleep_stage"),all.x = TRUE, all.y = TRUE)
 df = merge(df,dfa, by =c("id","sleep_stage"),all.x = TRUE, all.y = TRUE)
-
+df = merge(df,dfp, by =c("id","sleep_stage"),all.x = TRUE, all.y = TRUE)
 
 ######## removing outliers ?
 df_out = df
@@ -67,7 +67,7 @@ row_sub = apply(df_out[,3:ncol(df_out)], 1, function(row) all(row !=0 ))
 df_out = df_out[row_sub,]
 ########
 
-f_RandomForest = randomForest(sleep_stage~.,data=df[,2:ncol(df)],mtry = 86)
+f_RandomForest = randomForest(sleep_stage~.,data=df[,2:ncol(df)],mtry = 108)
 print(f_RandomForest)
 
 
@@ -81,7 +81,7 @@ f_RandomForest2 = randomForest(sleep_stage~.,
 print(f_RandomForest2)
 
 f_RandomForest2_o = randomForest(sleep_stage~.,
-                               data=df[,c("sleep_stage",rownames(subset(imp,imp[,1] > 150)))],ntree=700,mtry = 30)
+                               data=df[,c("sleep_stage",rownames(subset(imp,imp[,1] > 100)))],ntree=700,mtry = 48)
 print(f_RandomForest2_o)
 
 
@@ -90,6 +90,7 @@ print(f_RandomForest2_o)
 dft = rassembler_feat(train = FALSE) ##ent R et sd sur vaguelettes
 #dft = rassembler_feat_wave3(train = FALSE)  ##ent R et mean abs sur vaguelettes apres filtrage 0 30
 df_alp = rassembler_feat_alpha(train = FALSE)
+dftp = rassembler_feat_prop(train = FALSE)
 #sd_acc_t = read.csv(paste0(data_folder,"basic_feat_test.csv"))[,c(2,4,6,22)]
 abs_t = read.csv(paste0(data_folder,"basic_abs_test.csv"))
 dftest = rassembler_feat_freq(train = FALSE)
@@ -98,6 +99,7 @@ dftest[is.na(dftest)] = 0 #setting NA values to zero
 dftest = cbind(dftest,dft)
 dftest = cbind(dftest,abs_t)
 dftest = cbind(dftest,df_alp)
+dftest = cbind(dftest,dftp)
 
 
 
@@ -105,7 +107,7 @@ ytest = as.data.frame(predict(f_RandomForest2_o,dftest[,rownames(subset(imp,imp[
 ytest = cbind(yrandom[,1],ytest)
 colnames(ytest) =  c("id","sleep_stage")
 
-write.csv(ytest,file = paste0(data_folder,"ytest_freq_wave_alp.csv"),row.names = FALSE)
+write.csv(ytest,file = paste0(data_folder,"ytest_freq_wave_alp_prop.csv"),row.names = FALSE)
 #write.csv(ytest,file = paste0("ytest_freq_prop3.csv"),row.names = FALSE)
 
 
@@ -114,6 +116,7 @@ write.csv(ytest,file = paste0(data_folder,"ytest_freq_wave_alp.csv"),row.names =
 # calcul de ecart type et entropie de renyi sur ondelettes et sd 
 # amplitudes relatives des ondes alpha, theta etc après filtration
 # features sur ondes alpha seulement
+# proportions des ondes alpha etc
 # basic features : mean max min of absolute signal
 # RF
 
